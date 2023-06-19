51d8b13ed8400b5b194fa387bb760e843aac2aaa
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-06-19 16:42:06 +0600

    Create Feature Product Section in Home Page

--- src/Pages/Home/FeatureProducts/FeatureProducts.jsx
+++ src/Pages/Home/FeatureProducts/FeatureProducts.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import ProductCard from '../../Shared/ProductCard/ProductCard';
 
 const FeatureProducts = () => {
+    const [Fproducts, setFproducts] = useState();
+
+    useEffect( () => {
+         fetch('products.json')
+         .then(res => res.json())
+         .then(data => {
+            const featureProducts = data.filter(item => item.price < 1)
+            setFproducts(featureProducts)
+         })
+    }, [])
+
     return (
-        <div className='mt-20 mb-16'>
-            <SectionTitle topH={"Feature Products"} head={"Our Products"} topB={"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia"}></SectionTitle>
-        </div>
+        <>
+            <div className='mt-20 mb-16'>
+                <SectionTitle topH={"Feature Products"} head={"Our Products"} topB={"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia"}></SectionTitle>
+            </div>
+            <div className='grid grid-cols-3 gap-10 w-[1140px] mx-auto'>
+                {
+                    Fproducts.map(Fproduct => <ProductCard 
+                        key={Fproduct.id}
+                        Fproduct={Fproduct}
+                        ></ProductCard>)
+                }
+            </div>
+        </>
     );
 };
 
---
+++ src/Pages/Shared/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProductCard = ({ Fproduct }) => {
+    console.log(Fproduct, 4)
+    const { image, vegetableName, price } = Fproduct;
+    return (
+        <div>
+            <div className=' w-[285px] h-[328px] border'>
+                <img src={image} alt="" />
+                <div className='text-center'>
+                    <h1 className='text-2xl mt-4 mb-3'>{vegetableName}</h1>
+                    <h2>Price: $<span className='text-green-400 '>{price}</span></h2>
+                    <button className='btn btn-primary btn-sm mt-3'>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
